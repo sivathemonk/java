aa8a6ab8a501fa3eb1cab5a19c870e5e0f92d7a6
sivathemonk <sivathemonk@gmail.com>
2017-09-16 23:10:46 +0530

    This is just for testing

--- projectGitLearning/src/com/java/projectGit/Sairam.java
+++ projectGitLearning/src/com/java/projectGit/Sairam.java
@@ -4,8 +4,7 @@ public class Sairam {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Sairam");
-
+		System.out.println("Sairam "); // this is for testing git movement
 	}
 
 }
