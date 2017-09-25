22be3e2aa1a98168a217192675c56aeab746917d
sivathemonk <sivathemonk@gmail.com>
2017-09-25 14:14:58 +0530

    Sairam first program

---
+++ Sairam/src/com/java/sairam/Sairam.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.java.sairam;
+
+/**
+ * @author HP
+ *
+ */
+public class Sairam {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sairam");
+
+	}
+
+}
