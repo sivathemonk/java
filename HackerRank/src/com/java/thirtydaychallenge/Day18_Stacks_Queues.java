f238c516921dc76d1632c7fc554b5385d3d74b49
sivathemonk <sivathemonk@gmail.com>
2017-09-26 15:48:18 +0530

    modified as on 26092017

--- HackerRank/src/com/java/thirtydaychallenge/Day18_Stacks_Queues.java
+++ HackerRank/src/com/java/thirtydaychallenge/Day18_Stacks_Queues.java
@@ -15,24 +15,45 @@ public class Day18_Stacks_Queues {
 	/**
 	 * @param args
 	 */
+	Stack st = new Stack();
+	Queue<Character> q = new LinkedList<Character>();
+	void enqueueCharacter(char ch) {
+		q.add(ch);
+	}
+	void pushCharacter(char ch) {
+		st.push(ch);
+	}
+	char dequeueCharacter() {
+		return q.poll();
+	}
+	char popCharacter() {
+		return (char) st.pop();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Stack st = new Stack();
-		Queue<Character> q = new LinkedList<Character>();
-		String test = "shiva";
+		
+		Day18_Stacks_Queues sol = new Day18_Stacks_Queues();
+		String test = "sons";
 		char[] testarray = test.toCharArray();
-		for(int i=0; i < testarray.length; i++)
-			st.push(testarray[i]);
+		for(char c : testarray) {
+			sol.pushCharacter(c);
+			sol.enqueueCharacter(c);
+		}
+		
+		
 		//for(int i = 0; i <  testarray.length; i++)
 		//	System.out.print(st.pop() + "\t");
-		for(int i = 0; i <  testarray.length; i++)
-			q.add(testarray[i]);
+		
 		System.out.print("\n");
 		for(int i = 0; i < testarray.length; i++)
-				if(q.poll() == st.pop())
-					System.out.println("Palindrome");
+				if(sol.popCharacter() != sol.dequeueCharacter()) {
+					System.out.println(" not a Palindrome");
+					break;
+				}
+					
+		
 				else {
-					System.out.println("not a palindrome");
+					System.out.println("palindrome");
 					break;
 				}
 					
