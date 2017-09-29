f9ac1a20621f692a5cc315b3fb1ed22c4175f66b
sivathemonk <sivathemonk@gmail.com>
2017-09-29 17:36:26 +0530

    Adding all the unsaved programs from BST

---
+++ HackerRank/src/hackerRankPractise/BinarySearchTree.java
@@ -0,0 +1,5 @@
+package hackerRankPractise;
+
+public class BinarySearchTree {
+
+}
---
+++ HackerRank/src/hackerRankPractise/NonEmptyBST.java
@@ -0,0 +1,21 @@
+package hackerRankPractise;
+//url : https://www.hackerrank.com/challenges/30-binary-search-trees/tutorial
+
+public class NonEmptyBST<D extends Comparable> implements Tree  {
+	D Data;
+	Tree left;
+	Tree right;
+	public boolean isEmpty() {
+		return false;
+	}
+	NonEmptyBST() {
+		
+	}
+	
+	public boolean member(D elt) {
+		if(elt == Data) {
+			
+		}
+	}
+	
+}
