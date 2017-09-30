480e5bc526bd57bfbc1d3fc4a4b3a25d80d5cc8b
sivathemonk <sivathemonk@gmail.com>
2017-09-30 20:00:46 +0530

    adding all the BSTmodified

--- HackerRank/src/hackerRankPractise/NonEmptyBST.java
+++ HackerRank/src/hackerRankPractise/NonEmptyBST.java
@@ -1,21 +1,57 @@
 package hackerRankPractise;
 //url : https://www.hackerrank.com/challenges/30-binary-search-trees/tutorial
 
-public class NonEmptyBST<D extends Comparable> implements Tree  {
+public class NonEmptyBST<D extends Comparable> implements Tree<D>  {
 	D Data;
-	Tree left;
-	Tree right;
+	Tree<D> left;
+	Tree<D> right;
 	public boolean isEmpty() {
 		return false;
 	}
-	NonEmptyBST() {
+	NonEmptyBST(D elt, Tree<D> leftTree, Tree<D> rightTree) {
+		Data = elt;
+		left = leftTree;
+		right = rightTree;
 		
 	}
 	
-	public boolean member(D elt) {
+	public boolean  member(D elt) {
 		if(elt == Data) {
-			
+			return true;
+		}
+		else {
+			if(elt.compareTo(Data) < 1) {
+				return left.member(elt);
+			}
+			else {
+				return right.member(elt);
+			}
 		}
 	}
+	public int cardinality() {
+		return 1+left.cardinality()+right.cardinality();
+	}
+	
+	public NonEmptyBST<D> add(D elt) {
+		if(Data == elt) {
+			return this;
+		} else {
+			if(elt.compareTo(Data) < 0) {
+				return new NonEmptyBST(Data, left.add(elt),right);
+			}
+			else {
+				return new NonEmptyBST(Data, left, right.add(elt));
+			}
+		}
+	}
+	//NonEmptyBST() 
+//	@Override
+//	public int cardinality() {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	}
+
+	
+	
 	
 }
