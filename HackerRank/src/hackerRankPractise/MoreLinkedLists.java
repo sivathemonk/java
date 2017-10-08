3b1bf4e45ce483a8389be0b5a66d6d204e894827
Shiva <shiva@localhost.localdomain>
2017-10-08 19:04:36 +0530

    More Linked Lists

--- HackerRank/src/hackerRankPractise/MoreLinkedLists.java
+++ HackerRank/src/hackerRankPractise/MoreLinkedLists.java
@@ -25,6 +25,30 @@ public class MoreLinkedLists {
 	/**
 	 * @param args
 	 */
+	
+	public static Node_MLT removeDuplicates(Node_MLT head) {
+		Node_MLT start = head;
+		if(start == null || start.next == null) {
+			return start;
+		}
+		else {
+			if(start.data == start.next.data) {
+				start.next = start.next.next;
+				removeDuplicates(start);
+			}
+			else {removeDuplicates(start.next);}
+		}
+		/*while(start.next!=null) {
+			if(start.next.data == start.data){
+				//Node_MLT cur;
+				start = start.next;
+				//start.next = cur;
+			}
+			start = start.next;
+			}*/
+		return start;
+		
+	}
 	public static Node_MLT insert(Node_MLT head, int data) {
 		if(head == null)
 			return new Node_MLT(data);
@@ -61,6 +85,7 @@ public class MoreLinkedLists {
 			int ele = sc.nextInt();
 			head = insert(head,ele);
 		}
+		removeDuplicates(head);
 		display(head);
 	}
 
