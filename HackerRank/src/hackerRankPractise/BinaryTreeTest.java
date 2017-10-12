28a00f26a2ac9b76a7c7e090476b204903979c9c
sivathemonk <sivathemonk@gmail.com>
2017-10-12 14:43:28 +0530

    New file

---
+++ HackerRank/src/hackerRankPractise/BinaryTreeTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package hackerRankPractise;
+
+/**
+ * @author HP
+ *
+ */
+public class BinaryTreeTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tree t = new EmptyBST();
+		t.add("shiva");
+		System.out.println(t.isEmpty());
+
+	}
+
+}
