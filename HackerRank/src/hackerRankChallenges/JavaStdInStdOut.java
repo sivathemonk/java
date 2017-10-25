3eac1ad8f258911615afd7961b447a2ca879ce32
root <root@localhost.localdomain>
2017-10-26 00:13:14 +0530

    adding two more things

---
+++ HackerRank/src/hackerRankChallenges/JavaStdInStdOut.java
@@ -0,0 +1,31 @@
+/**
+//url: https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem
+ * 
+ */
+package hackerRankChallenges;
+
+import java.util.*;
+
+/**
+ * @author root
+ *
+ */
+public class JavaStdInStdOut {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
