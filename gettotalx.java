dec451460980141d1d55cc1a6556d857c698d3d2
sivathemonk <sivathemonk@gmail.com>
2019-07-10 08:18:23 +0530

    gettotalx

--- gettotalx.java
+++ gettotalx.java
@@ -3,17 +3,19 @@ import java.util.* ;
 
 class gettotalx{
 
-int lcmfun(arr){
+static int lcmfun(int[] arr){
 int max = arr[0];
 int lcm = 0;
+int prod = 1;
 for (int i = 1 ; i < arr.length ; i ++){
 if(arr[i]>max)
 max = arr[i];
-
+prod = prod * arr[i];
 }
+prod = prod * arr[0];
 //while(lcm!=0)
 condition :
-for(int j = 0; j <arr.length ; j ++){
+for(int j = 1; j < prod ; j ++){
 for(int i = 0 ; i < arr.length ; i ++){
 if((max*j)%arr[i] == 0 ){
 lcm = max*j;
@@ -24,10 +26,26 @@ break condition;
 return lcm;
 }
 
-int result(int[] arr1, int arr2){
-lcm = lcmfun(arr1);
+static int result(int[] arr1, int[] arr2){
+int count = 0;
+int lcm = lcmfun(arr1);
+int min = arr2[0];
+for( int i=1; i < arr2.length ; i++ ){
+	if(arr2[i] < min){ min = arr2[i] ; }
+}
+for( int i = 1 ; i < min ; i++ ){
+	int cond = 0;
+	for(int j = 1 ; j < arr2.length ; j++ ){
+		if(arr2[j] % (lcm*i) == 0) {cond = 1;break;}
+		else { cond = 0; }
+	}
+	if(cond == 1){ count ++; System.out.println(lcm*i);}
+}
+	return count ;
 
 }
+
+
 
 public static void main(String[] args){
 
@@ -39,12 +57,28 @@ String arr1 = sc.nextLine();
 String arr2 = sc.nextLine();
 
 //splitting the strings
-int sizearr[] = size.split(" ");
-int arr1num = arr1.split(" ");
-int arr2num = arr2.split(" ");
+String[] sizearr = size.split(" ");
+String[] arr1num = arr1.split(" ");
+String[] arr2num = arr2.split(" ");
 
+//Making integer arrays
+int s = sizearr.length;
+int[] sizearrnum = new int[s];
+for(int i=0 ; i < s ; i++ ){
+	sizearrnum[i] = Integer.parseInt(sizearr[i]);
+}
+int[] arr1num1 = new int[arr1num.length];
+for(int i = 0; i < arr1num.length ; i++ ){
+	arr1num1[i] = Integer.parseInt(arr1num[i]);
+	//System.out.print(arr1num1[i]);
+}
+int[] arr2num1 = new int[arr2num.length];
+for(int i = 0; i< arr2num.length ; i ++){
+	arr2num1[i] = Integer.parseInt(arr2num[i]);
+	//System.out.print(arr2num1[i]);
+}
 //total integers
-int total = gettotalx.result(arr1num, arr2num);
+int total = gettotalx.result(arr1num1, arr2num1);
 
 System.out.println(total);
 }
